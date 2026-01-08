8ba430c792151ab1716a68641078dbffe9050a76
Chaeyeon Han <strawberry@khu.ac.kr>
2026-01-09 07:31:28 +0900

    feat:add videosection that accidently deleted

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -2,6 +2,7 @@ import { HeroSection } from "./components/HeroSection";
 import { OurStorySection } from "./components/OurStorySection";
 import BetaSignUpSection from "./components/BetaSignupSection";
 import { ScrollManager } from "@/components/behavior/ScrollManager";
+import { VideoSection } from "./components/VideoSection";
 
 export default function HomeView() {
   return (
@@ -12,6 +13,7 @@ export default function HomeView() {
       <HeroSection />
       <OurStorySection />
       <BetaSignUpSection />
+      <VideoSection />
     </>
   );
 }
