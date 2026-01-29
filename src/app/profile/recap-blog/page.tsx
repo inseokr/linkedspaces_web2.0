08a8f2417a5922d83dfac5f2ed394ab279567fc1
Chaeyeon Han <strawberry@khu.ac.kr>
2026-01-30 02:42:07 +0900

    fix: wrap ProfileRecapBlogsView in Suspense to fix build error

--- src/app/profile/recap-blog/page.tsx
+++ src/app/profile/recap-blog/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import ProfileRecapBlogsView from "@/views/Profile/recap-blogs";
 
-export default function ProfileRecapPage() {
-  return <ProfileRecapBlogsView />;
+export default function RecapBlogPage() {
+  return (
+    <Suspense fallback={<div>Loading recap blogs...</div>}>
+      <ProfileRecapBlogsView />
+    </Suspense>
+  );
 }
