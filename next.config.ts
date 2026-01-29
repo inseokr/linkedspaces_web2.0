2fe38e35ec6eb9dc6f8d0def4cf2f355870a1a4c
Chaeyeon Han <strawberry@khu.ac.kr>
2026-01-30 01:24:40 +0900

    feat: data integration on map : blog detail page

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,12 @@ const nextConfig: NextConfig = {
         hostname: "pocketverse.herokuapp.com",
         pathname: "/public/user_resources/**",
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/a/**",
+      },
     ],
   },
 };
