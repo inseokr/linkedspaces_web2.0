9a2d526a84eef91a56ec4fc538808cc55c7ec84f
linkedspaces <inseo.kr@linkedspaces.com>
2026-02-19 17:10:50 -0800

    Redesign Home Hero section and add hero_theme.css

--- src/views/LearnMore/components/BetaSignupSection.tsx
+++ src/views/LearnMore/components/BetaSignupSection.tsx
@@ -2,11 +2,11 @@ import BetaSignUpCard from "@/components/BetaSignupCard";
 
 export function BetaSignUpSection() {
   return (
-    <div className="mt-13 justify-center items-center flex flex-col font-semibold bg-white text-black">
-      <h1 className="text-[56px] [font-family:var(--font-poppins)] text-black">
+    <div className="mt-13 justify-center items-center flex flex-col font-semibold">
+      <h1 className="text-[56px] [font-family:var(--font-poppins)] text-white">
         LinkedSpaces Beta
       </h1>
-      <p className="mt-6 items-center text-base text-black">
+      <p className="mt-6 items-center text-base text-white/80">
         Sign Up and Thank Yourself Later!
       </p>
       <BetaSignUpCard />
