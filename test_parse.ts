bd60170319a15c990111f994c1195da578f3eeb8
inseokr <inseo.kr@gmail.com>
2026-02-24 12:35:45 -0800

    #deleting unnecessary files

--- test_parse.ts
+++
@@ -1,4 +0,0 @@
-import { formatTripDateRangeLabel } from "./src/utils/formatTripDate";
-console.log(formatTripDateRangeLabel("July 18", "July 20"));
-console.log(formatTripDateRangeLabel("July 18, 2025", "July 20, 2025"));
-console.log(formatTripDateRangeLabel("Dec 12", "Dec 25"));
